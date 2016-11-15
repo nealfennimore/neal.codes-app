251ed6bccf1cb64756d33708ff2531cf14e4c284
nealfennimore <hi@neal.codes>
2016-11-15 10:15:33 -0800

    Fix build config script paths

--- scripts/buildConfig.sh
+++ scripts/buildConfig.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-DOCKER_ROOT="../.."
-APP_ROOT="../"
+FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+_DOCKER_ROOT="${FILE_DIR}/../.."
+_APP_ROOT="${FILE_DIR}/.."
 
-source "${DOCKER_ROOT}/utils.sh"
-
+source "${_DOCKER_ROOT}/utils.sh"
 export $(getEnvVars)
 
 env_names=$(getEnvSubstituteString)
 
-envsubst $env_names < "${DOCKER_ROOT}/docker/containers/app/config.js" > "${APP_ROOT}/config.js"
+envsubst $env_names < "${_DOCKER_ROOT}/docker/containers/app/config.js" > "${_APP_ROOT}/config.js"
