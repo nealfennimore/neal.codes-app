84afd5f77f077cfbf92e6a98dc423c2ff6c78bbc
Neal Fennimore <hi@neal.codes>
2016-11-20 22:49:36 -0800

    Add in redux thunk to client. Add in redux dev tools when in dev mode

--- client/app.js
+++ client/app.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
-
-import Routes from 'routes';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from 'reducers';
+import Routes from 'routes';
+
+const reduxDevtoolsCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = process.env.NODE_ENV === 'development' && reduxDevtoolsCompose ? reduxDevtoolsCompose : compose;
 
 // Grab the state from a global injected into server-generated HTML
 const preloadedState = window.__PRELOADED_STATE__;
-const store = createStore(reducers, preloadedState);
+const store = createStore(reducers, preloadedState, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 syncHistoryWithStore(browserHistory, store);
 
