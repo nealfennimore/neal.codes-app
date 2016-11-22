bb516c8dc24ebb20b52d18a4f0c92f7caad101d7
Neal Fennimore <hi@neal.codes>
2016-11-21 17:17:59 -0800

    Minor linter fix

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -3,12 +3,12 @@ import { routerReducer } from 'react-router-redux';
 
 function counter(state = 0, action) {
     switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
-        default:
-            return state
+    case 'INCREMENT':
+        return state + 1;
+    case 'DECREMENT':
+        return state - 1;
+    default:
+        return state;
     }
 }
 
