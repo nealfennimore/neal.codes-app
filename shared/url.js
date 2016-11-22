2afa785a0a3815826d624ff9cca1a8932c5b0999
Neal Fennimore <hi@neal.codes>
2016-11-21 19:37:59 -0800

    Add in shared url

---
+++ shared/url.js
@@ -0,0 +1,20 @@
+import { isBrowser } from 'shared/env';
+
+function constructParam(name, queryParams){
+    return `${name}=${isBrowser ? encodeURIComponent(queryParams[name]) : queryParams[name]}`;
+}
+
+export function constructParams(paramNames, queryParams){
+    return paramNames.map((name)=>constructParam(name, queryParams)).join('&');
+}
+
+export function constructURL(path, queryParams){
+    let paramNames = Object.keys(queryParams),
+        params     = '';
+
+    if(paramNames.length){
+        params = `?${constructParams(paramNames, queryParams)}`;
+    }
+
+    return `${path}${params}`;
+}
