a3509dff1f96b1cef34d0fd3919fe1793a44b5c1
Neal Fennimore <hi@neal.codes>
2016-11-27 16:23:19 -0800

    Add markdown to post and truncate

--- client/js/components/blog/posts/Post.jsx
+++ client/js/components/blog/posts/Post.jsx
@@ -1,10 +1,24 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
+const truncate = (str)=> {
+    const MAX_SIZE = 150;
+    const newStr = str.slice(0, MAX_SIZE).trim();
+
+    if(newStr.length >= MAX_SIZE){
+        return newStr.replace(/(\s*\w*)$/m, '...');
+    } else {
+        return newStr;
+    }
+};
+
 const Post = ({post}) => {
     return (
         <li>
             <Link to={`/blog/${post.slug}`}>{post.title}</Link>
+            <p>
+                {truncate(post.markdown)}
+            </p>
         </li>
     );
 };
