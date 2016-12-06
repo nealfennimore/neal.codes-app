e336a647c8ca40f1459719048851299af10c8782
Neal Fennimore <hi@neal.codes>
2016-12-06 00:05:09 -0800

    Update navigation to not be full width with menu items

--- client/js/components/global/Navigation.jsx
+++ client/js/components/global/Navigation.jsx
@@ -6,10 +6,12 @@ import styles from './Navigation.scss';
 
 const Navigation = (props) => {
     return (
-        <nav className={`${styles.navigation} row expanded align-middle align-justify`}>
-            <Logo />
-            <NavigationLinks />
-        </nav>
+        <div className={`${styles.container} row expanded`}>
+            <nav className={`${styles.navigation} row align-middle align-justify`}>
+                <Logo />
+                <NavigationLinks />
+            </nav>
+        </div>
     );
 };
 
