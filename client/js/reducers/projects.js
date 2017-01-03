1201c7f593f1db2d09433c3b5f7d53c510aed3a3
Neal Fennimore <hi@neal.codes>
2017-01-03 15:07:22 -0800

    Update projects reducer with HYM and hope card images

--- client/js/reducers/projects.js
+++ client/js/reducers/projects.js
@@ -1,12 +1,38 @@
+///////////////
+// HopeCards //
+///////////////
+import HopecardsCover from 'images/projects/Hopecards/hopecards-cover.jpg';
+import HopecardsHomepage from 'images/projects/Hopecards/hopecards-homepage.jpg';
+import HopecardsStep2 from 'images/projects/Hopecards/hopecards-step-2.jpg';
+import HopecardsStep3 from 'images/projects/Hopecards/hopecards-step-3.jpg';
+import HopecardsDedication from 'images/projects/Hopecards/hopecards-dedication-page.jpg';
+import HopecardsFavorites from 'images/projects/Hopecards/hopecards-favorites.jpg';
+import HopecardsImpact from 'images/projects/Hopecards/hopecards-impact.jpg';
+
+/////////
+// HYM //
+/////////
 import HYMCover from 'images/projects/HonorYourMom/HYM-cover.jpg';
+import HYMHomepage from 'images/projects/HonorYourMom/HYM-homepage.jpg';
+import HYMStep1 from 'images/projects/HonorYourMom/HYM-step-1.jpg';
+import HYMStep2 from 'images/projects/HonorYourMom/HYM-step-2.jpg';
+import HYMStep3 from 'images/projects/HonorYourMom/HYM-step-3.jpg';
+import HYMAbout from 'images/projects/HonorYourMom/HYM-about.jpg';
+import HYMHonoredMoms from 'images/projects/HonorYourMom/HYM-honored-moms.jpg';
+import HYMHonoredMom from 'images/projects/HonorYourMom/HYM-honored-mom.jpg';
 
 const initialState = [
     {
         title: 'Hope Cards',
         subtitle: 'Greeting cards that help save lifes',
-        coverImage: HYMCover,
+        coverImage: HopecardsCover,
         images: [
-            HYMCover
+            HopecardsHomepage,
+            HopecardsStep2,
+            HopecardsStep3,
+            HopecardsDedication,
+            HopecardsFavorites,
+            HopecardsImpact
         ]
     },
     {
@@ -14,25 +40,31 @@ const initialState = [
         subtitle: 'A tribute to moms everywhere',
         coverImage: HYMCover,
         images: [
-            HYMCover
+            HYMHomepage,
+            HYMStep1,
+            HYMStep2,
+            HYMStep3,
+            HYMAbout,
+            HYMHonoredMoms,
+            HYMHonoredMom
         ]
     },
-    {
-        title: 'Concept Map',
-        subtitle: 'A tribute to moms everywhere',
-        coverImage: HYMCover,
-        images: [
-            HYMCover
-        ]
-    },
-    {
-        title: 'Teacher Dashboard',
-        subtitle: 'A tribute to moms everywhere',
-        coverImage: HYMCover,
-        images: [
-            HYMCover
-        ]
-    },
+    // {
+    //     title: 'Concept Map',
+    //     subtitle: 'A tribute to moms everywhere',
+    //     coverImage: HYMCover,
+    //     images: [
+    //         HYMCover
+    //     ]
+    // },
+    // {
+    //     title: 'Teacher Dashboard',
+    //     subtitle: 'A tribute to moms everywhere',
+    //     coverImage: HYMCover,
+    //     images: [
+    //         HYMCover
+    //     ]
+    // },
 ];
 
 
