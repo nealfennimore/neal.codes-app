dd67c16cf51f6ab94571cec1e7e81661abe98a0a
Neal Fennimore <hi@neal.codes>
2017-01-07 23:22:10 -0800

    Update home styles. Add in social icons

--- client/js/containers/Home.jsx
+++ client/js/containers/Home.jsx
@@ -8,10 +8,38 @@ export default class Home extends Component {
         return (
             <div className={`${styles.home} row align-middle align-center`}>
                 <div className='column small-10'>
-                    <h1>Neal Fennimore</h1>
-                    <h2 className='subheader'>Front End Developer</h2>
-                    <p>I'm a full-stack web developer specializing in design and front-end engineering. I'm a polyglot programmer versed in Javascript, Ruby, PHP, and HTML/CSS. I have experience building with front-end frameworks Backbone and React, building single-page applications, and creating data visualization in D3 and CSS3.</p>
+
+                    <div className={`row ${styles.summary}`}>
+                        <div className='column'>
+                            <h1>Neal Fennimore</h1>
+                            <h2 className='subheader'>Front End Developer</h2>
+                            <p>I'm a full-stack web developer specializing in design and front-end engineering. I'm a polyglot programmer versed in Javascript, Ruby, PHP, and HTML/CSS. I have experience building with front-end frameworks Backbone and React, building single-page applications, and creating data visualization in D3 and CSS3.</p>
+                        </div>
+                    </div>
+
+                    <div className='row align-center text-center'>
+                        <div className='column small-2'>
+                            <Link to='https://github.com/nealfennimore'>
+                                <i className='icon-github'></i>
+                                <span>Github</span>
+                            </Link>
+                        </div>
+                        <div className='column small-2'>
+                            <Link to='https://www.linkedin.com/in/nealfennimore'>
+                                <i className='icon-linkedin'></i>
+                                <span>Linkedin</span>
+                            </Link>
+                        </div>
+                        <div className='column small-2'>
+                            <Link to='/blog'>
+                                <i className='icon-ion-social-rss'></i>
+                                <span>Blog</span>
+                            </Link>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
         );
     }
