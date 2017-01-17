873b5af4df6cf1d157feb1ef3748c688680987ef
Neal Fennimore <hi@neal.codes>
2017-01-17 14:08:28 -0800

    Update vertical rhythm sitewide

--- client/js/components/blog/common/PostLeft.jsx
+++ client/js/components/blog/common/PostLeft.jsx
@@ -7,7 +7,7 @@ import styles from './Post.scss';
 const PostLeft = ({post}) => {
     return (
         <div className={`${styles.postLeft} column small-12 medium-8`}>
-            <h2><Link to={`/blog/${post.slug}`}>{post.title}</Link></h2>
+            <h2 className='h3'><Link to={`/blog/${post.slug}`}>{post.title}</Link></h2>
             <p>
                 {cleanMarkdown(truncate(post.markdown, 170))}
                 <Link to={`/blog/${post.slug}`} className={styles.readMore}>&#187;</Link>
