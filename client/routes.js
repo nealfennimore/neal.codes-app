c82fbc8a1a159a2bc9d8894d2d8cfd69374c4830
Neal Fennimore <hi@neal.codes>
2017-01-19 22:44:08 -0800

    Add in google analytics page view logging on router change:

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {Router, Route, IndexRoute} from 'react-router';
 import { Layout, Home, Blog, Projects } from 'containers';
 import { Post, Tags } from 'components/blog';
 import { NotFound } from 'components/common';
+import { logPageView } from 'lib/googleAnalytics';
 
 // Fixes HMR by not recreating routes
 // https://github.com/reactjs/react-router-redux/issues/179#issuecomment-241771171
@@ -23,7 +24,7 @@ const ROUTES = (
 
 export default function Routes({history}) {
     return (
-        <Router history={history}>
+        <Router history={history} onUpdate={logPageView}>
             {ROUTES}
         </Router>
     );
