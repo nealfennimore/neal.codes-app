373c7d320b4d44435c688190180dddf885bd3551
Neal Fennimore <hi@neal.codes>
2017-03-25 19:32:01 -0700

    Add in 404 spec

---
+++ client/js/components/common/404.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+import NotFound from './404.jsx';
+import styles from './404.scss';
+
+describe('<404/>', function() {
+    it('has the right class', function() {
+        const wrapper = shallow(<NotFound/>);
+        const classNames = styles._404.replace(/\s+/, '.');
+        expect(wrapper.find(`.${classNames}`)).to.have.length(1);
+    });
+});
