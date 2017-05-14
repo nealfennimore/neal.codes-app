c9914b71d4d47551285447f59e44069a7299ac37
Neal Fennimore <hi@neal.codes>
2017-05-13 18:30:35 -0700

    Add in client store

--- client/middleware.js
+++ client/middleware.js
@@ -2,14 +2,18 @@ import { compose } from 'redux';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
 import {browserHistory} from 'react-router';
+import createSagaMiddleware from 'redux-saga'
 import { isDevelopment, isBrowser } from 'shared/env';
 
 // Add in redux dev tools when in development
 const reduxDevtoolsCompose    = isBrowser && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 export const composeEnhancers = isDevelopment && reduxDevtoolsCompose ? reduxDevtoolsCompose : compose;
 
+export const sagaMiddleware   = createSagaMiddleware();
+
 const defaultMiddleware = [
     routerMiddleware(browserHistory),
+    sagaMiddleware,
     thunk
 ];
 
