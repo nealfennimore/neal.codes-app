32c3f1df0d025d1351dcf4dbb5d5cc09830aa02f
Neal Fennimore <hi@neal.codes>
2017-05-26 00:22:43 -0700

    Minor update to middleware

--- client/middleware.js
+++ client/middleware.js
@@ -8,7 +8,7 @@ import { isDevelopment, isBrowser } from 'shared/env';
 const reduxDevtoolsCompose    = isBrowser && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 export const composeEnhancers = isDevelopment && reduxDevtoolsCompose ? reduxDevtoolsCompose : compose;
 
-export const sagaMiddleware   = createSagaMiddleware();
+export const sagaMiddleware = createSagaMiddleware();
 
 const defaultMiddleware = [
     routerMiddleware(browserHistory),
