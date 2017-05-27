6c50f8f628f57620c90f560a5e276e69246712b2
Neal Fennimore <hi@neal.codes>
2017-05-26 18:47:26 -0700

    Fix title not rendering correctly

--- server/templates/head/index.js
+++ server/templates/head/index.js
@@ -7,7 +7,7 @@ export default function head({helmet}){
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
-            <title>${helmet.title.toString()}</title>
+            ${helmet.title.toString()}
             ${helmet.meta.toString()}
             ${helmet.link.toString()}
 
