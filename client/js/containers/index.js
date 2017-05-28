b95a10b9e2810f1642c772ca7d4d0004c5612571
Neal Fennimore <hi@neal.codes>
2017-05-28 15:06:12 -0700

    Remove projects reducer from initial load

--- client/js/containers/index.js
+++ client/js/containers/index.js
@@ -4,4 +4,3 @@ export { default as Blog } from 'containers/Blog';
 export { default as Post } from 'containers/blog/Post';
 export { default as Posts } from 'containers/blog/Posts';
 export { default as Tags } from 'containers/blog/Tags';
-export { default as Projects } from 'containers/Projects';
--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import blog from 'reducers/blog';
-import projects from 'reducers/projects';
 
 export default combineReducers({
     blog,
-    projects,
     routing: routerReducer
 });
