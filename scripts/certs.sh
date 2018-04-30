bd4c3600abb9b9c64072f62a8db07ee2b0437a6a
Neal Fennimore <hi@neal.codes>
2018-04-29 19:01:46 -0700

    Fix cert script

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -1,3 +1,1 @@
-openssl req \
-    -newkey rsa:2048 -nodes -keyout "certs/$1.key "\
-    -out "certs/$1.csr"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "certs/$1.key" -out "certs/$1.crt"
