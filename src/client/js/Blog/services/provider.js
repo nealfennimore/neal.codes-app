7b186538b53a03ebb2d005ef9e2764e0f57fcf9b
Neal Fennimore <hi@neal.codes>
2018-05-07 22:31:15 -0700

    Fix node server host

--- src/client/js/Blog/services/provider.js
+++ src/client/js/Blog/services/provider.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { HOST } from 'shared/config';
+import { __SERVER__ } from 'shared/env';
+import { HOST, WEB_HOST } from 'shared/config';
+
+const host = __SERVER__ ? WEB_HOST : HOST;
 
 export default function provider() {
     return axios.create( {
-        baseURL: `${HOST}/api`
+        baseURL: `${host}/api`
     } );
 }
