a5e90595e9057ed6f62840ea077806fb6fa9c899
Neal Fennimore <hi@neal.codes>
2018-05-24 19:25:57 -0700

    Add helmet to renderer

--- src/server/renderer.js
+++ src/server/renderer.js
@@ -3,6 +3,7 @@ import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router';
 import { Provider } from 'react-redux';
 import Loadable from 'react-loadable';
+import { Helmet } from 'react-helmet';
 import { preloadQueue } from '@nfen/redux-saga-injector';
 import serialize from 'serialize-javascript';
 import App from 'client/js/Global/components/App.jsx';
@@ -74,10 +75,19 @@ export default async function render( req, res ) {
     // Get dynamic bundles from code splits needed for this render
     const bundle = getBundleTags( modules );
 
+    const helmet = Helmet.renderStatic();
+
     res.send( `
         <!DOCTYPE html>
-        <html>
+        <html ${helmet.htmlAttributes.toString()}>
             <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link href="https://google.com/favicon.ico" rel="icon" />
+
+                ${helmet.title.toString()}
+                ${helmet.meta.toString()}
+                ${helmet.link.toString()}
+
                 ${bundle.styles}
                 ${styles}
                 ${manifest}
@@ -92,9 +102,8 @@ export default async function render( req, res ) {
                     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
                     })(document);
                 </script>
-                <link href="https://google.com/favicon.ico" rel="icon" />
             </head>
-            <body>
+            <body ${helmet.bodyAttributes.toString()}>
                 <div id="app">${html}</div>
                 <script>
                     window.__PRELOADED_STATE__ = ${ serialize( store.getState(), { isJSON: true } ) };
