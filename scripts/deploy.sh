203dba06ff6dbce130f1d7bbf3af5136884cf032
Neal Fennimore <hi@neal.codes>
2018-07-23 19:54:19 -0700

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+cd $PWD/..
+ln -sf .env.production.sample .env
+eval "$(docker-machine env $DIGITAL_OCEAN_NEAL_CODES)"
+docker-machine ssh $DIGITAL_OCEAN_NEAL_CODES "cd /mnt/volume-sfo2-01/app && git pull"
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml 
