6c74fef4f95f30fa6428ec0227c4c1a23baa2228
Neal Fennimore <hi@neal.codes>
2018-07-23 19:55:51 -0700

    Add in docker logs to deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@ ln -sf .env.production.sample .env
 eval "$(docker-machine env $DIGITAL_OCEAN_NEAL_CODES)"
 docker-machine ssh $DIGITAL_OCEAN_NEAL_CODES "cd /mnt/volume-sfo2-01/app && git pull"
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml 
+docker-compose logs -f
